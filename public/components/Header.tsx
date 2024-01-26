577f06953af42f46fe9b3d8f936a186e1a56ec55
shashirith <iec2020069@iiita.ac.in>
2024-01-26 23:23:02 +0530

    fix:navbar

--- public/components/Header.tsx
+++ public/components/Header.tsx
@@ -1,10 +1,18 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <div>
-      header
-    </div>
+      <header className='flex items-centre justify-between h-20 max-width border-b border-peach'>
+      <Link href="/">
+       <h5 className='text-purple text-xl font-semibold'>Shashirith</h5>
+       </Link>
+       <nav>
+        <ul>
+          <li><Link href='/about'>about</Link></li>
+       </ul>
+       </nav>
+      </header>
   );
 };
 
